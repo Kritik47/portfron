59bcd127545f4776b8f6cfe6a8e588389d79495d
KRITIK KUMAR <Kritiksahu958958@gmail.com>
2024-08-20 17:15:35 +0530

    change

--- src/REDUXTOOLKIT/Store.js
+++
@@ -1,8 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit';
-import DataSlice from './Slice';
-const DataStore=configureStore({
-    reducer:{
-       DATA:DataSlice 
-    }
-})
-export default DataStore;
