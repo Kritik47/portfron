30a164059d503caf91a5870511d4c3d9617bb523
KRITIK KUMAR <Kritiksahu958958@gmail.com>
2024-09-19 11:47:27 +0530

    Restore src/MDMARTS folder

--- src/MDMARTS/ProductStore.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import ProductSlice from "./ProductSlice";
-const ProductStore=configureStore({
-    reducer:{
-        allpro:ProductSlice
-    }
-})
-export default ProductStore;
